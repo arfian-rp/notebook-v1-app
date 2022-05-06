8feeca62c0ee2713dbab210100263b82a8a4fe2b
arfian-rp <arfianpradana07@gmail.com>
2022-05-06 11:19:20 +0700

    ok

--- src/components/SettingsPopup.tsx
+++ src/components/SettingsPopup.tsx
@@ -1,34 +1,52 @@
 import React, { useState } from "react";
 
 interface Props {
-  saveHandl: (fileName: string) => void;
+  text: string;
   clearHandl: () => void;
   display: string;
 }
-export default function SettingsPopup({ saveHandl, clearHandl, display = "hidden" }: Props) {
+export default function SettingsPopup({ text, clearHandl, display = "hidden" }: Props) {
   const [fileName, setFileName] = useState<string>(`FILE-${new Date().toLocaleString()}.txt`);
 
+  function downloadHandl(e: React.FormEvent) {
+    e.preventDefault();
+    setFileName(`FILE-${new Date().toLocaleString()}.txt`);
+    const Data = btoa(text);
+    const a = document.createElement("a");
+    a.download = fileName;
+    a.href = `data:text/plain;base64,${Data}`;
+    a.click();
+    setTimeout(() => {
+      window.location.reload();
+    }, 250);
+  }
   return (
     <div style={{ display }} className="fixed border-2 border-secondary bg-primary w-[25rem] top-[25vh] left-[50%] transform -translate-x-[50%] h-fit">
       <div className="text-center m-auto">Settings</div>
       <hr />
-      <div className="m-6">
-        <div className="flex flex-col gap-10">
+      <form onSubmit={downloadHandl} className="m-6">
+        <div className="flex flex-col gap-7">
           <div className="flex justify-between items-center">
             <div>File name:</div>
             <input className="m-1 p-1 text-center text-primary rounded-md" value={fileName} onChange={(e) => setFileName(e.target.value)} type="text" autoFocus />
           </div>
           <div className="flex justify-between items-center">
+            <div>Word count:</div>
+            <div className="text-lg">{text.match(/(\w+)/g)?.length}</div>
+          </div>
+          <div className="flex justify-between items-center">
+            <div>Number of sentences:</div>
+            <div className="text-lg">{text.split(/[.!?]/).length - 1}</div>
+          </div>
+          <div className="flex justify-between items-center">
+            <div>Number of characters:</div>
+            <div className="text-lg">{text.length}</div>
+          </div>
+          <div className="flex justify-between items-center">
             <button onClick={clearHandl} className="p-2 px-5 rounded-md border-2 transition duration-300 ease-in-out bg-secondary text-primary hover:bg-primary hover:text-secondary hover:border-secondary">
               Clear
             </button>
-            <button
-              onClick={() => {
-                setFileName(`FILE-${new Date().toLocaleString()}.txt`);
-                saveHandl(fileName);
-              }}
-              className="p-2 px-5 rounded-md border-2 transition duration-300 ease-in-out bg-secondary text-primary hover:bg-primary hover:text-secondary hover:border-secondary"
-            >
+            <button type="submit" className="p-2 px-5 rounded-md border-2 transition duration-300 ease-in-out bg-secondary text-primary hover:bg-primary hover:text-secondary hover:border-secondary">
               Download
             </button>
             <button
@@ -41,7 +59,7 @@ export default function SettingsPopup({ saveHandl, clearHandl, display = "hidden
             </button>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
